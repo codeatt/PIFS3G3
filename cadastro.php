f3b19039353be366cec1ed3ed2b454ee4da34d29
Otavio Camargo <ojcamargo@gmail.com>
2018-09-07 20:27:46 -0300

    correcoes cadastro

--- cadastro.php
+++ cadastro.php
@@ -21,51 +21,51 @@ include "comum/funcoes.php";
 	  $autorizacaoContato = isset($_POST["autorizacaoContato"]) ? $_POST["autorizacaoContato"] : "";
 
 	  if (empty($nome)) {
-		$erroNome = "Nome não informado";
+			$erroNome = "Nome não informado";
 	  }
 	  else if(strpos($nome, ' ') === false || strlen($nome) < 15) {
-		$erroNome = "Nome inválido";
+			$erroNome = "Nome inválido";
 	  }
 
 	  if(empty($email)) {
-		$erroEmail = "E-mail não informado";
+			$erroEmail = "E-mail não informado";
 	  }
 	  else if (validarEmail($email)) {
-		$erroNome = "E-mail inválido";
+			$erroNome = "E-mail inválido";
 	  }
 
 	  if (strlen($senha) <= '8') {
-		$erroSenha = "Deve ter no mínimo 8 caracteres";
+			$erroSenha = "Deve ter no mínimo 8 caracteres";
 	  }
 	  elseif(!possuiNumeros($senha)) {
-		$erroSenha = "Deve ter pelo menos um número";
+			$erroSenha = "Deve ter pelo menos um número";
 	  }
 	  elseif(!possuiLetrasMaiusculas($senha)) {
-		$erroSenha = "Deve ter pelo menos uma letra maiuscula";
+			$erroSenha = "Deve ter pelo menos uma letra maiuscula";
 	  }
 	  elseif(!possuiLetrasMinusculas($senha)) {
-		$erroSenha = "Deve ter pelo menos uma letra minuscula";
+			$erroSenha = "Deve ter pelo menos uma letra minuscula";
 	  }
 	  elseif(!possuiCaracterEspecialValido($senha)) {
-		$erroSenha = "Deve ter pelo menos um caracter especial";
+			$erroSenha = "Deve ter pelo menos um caracter especial";
 	  }
 
 	  if ($confirmacao !== $senha) {
-		$erroConfirmacao = "As senhas informadas devem ser iguais";
+			$erroConfirmacao = "As senhas informadas devem ser iguais";
 	  }
 
 	  if(empty($cpf)) {
-		$erroCpf = "CPF não informado";
+			$erroCpf = "CPF não informado";
 	  }
 	  else if(validarCPF($cpf)) {
-		$erroCpf = "CPF inválido";
+			$erroCpf = "CPF inválido";
 	  }
 
 	  if(empty($dataNascimento)) {
-		$erroDataNascimento = "Data não informada";
+			$erroDataNascimento = "Data não informada";
 	  }
 	  else if(!validarData($dataNascimento)) {
-		$erroDataNascimento = "Data inválida";
+			$erroDataNascimento = "Data inválida";
 	  }
 
 	  if(!empty($celular)) {
@@ -87,11 +87,11 @@ include "comum/funcoes.php";
 			$arquivoTemporario = $_FILES["foto"]["tmp_name"];
 			$caminho = "fotos/$nomeArquivo";
 
-			if(!validarArquivo($nomeArquivo,array['png','jpg'])) {
-				// $erroArquivo = "Somente arquivos do tipo PNG ou JPG";
+			if(!validarArquivo($nomeArquivo,array('png','jpg'))) {
+				 $erroArquivo = "Somente arquivos do tipo PNG ou JPG";
 			}
-	//
-	// 		$status = move_uploaded_file($arquivoTemporario, $caminho);
+
+	 		$status = move_uploaded_file($arquivoTemporario, $caminho);
 	 	}
 	}
 
@@ -142,57 +142,58 @@ include "comum/funcoes.php";
           <div class="col-md-12 form-group">
             <label class="form-label-required" for="nome">Nome Completo:</label>
             <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite seu nome completo aqui" value="<?php echo(isset($nome) ? $nome : '') ?>" required />
-			<span class="error"><?php echo $erroNome;?></span>
+						<span class="error"><?php echo isset($erroNome) ? $erroNome : "";?></span>
           </div>
         </div>
         <div class="row">
           <div class="col-md-12 form-group">
             <label class="form-label-required" for="email">Email:</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Ex.: email@dominio.com" value="<?php echo(isset($email) ? $email : '') ?>" required />
-			<span class="error"><?php echo $erroEmail;?></span>
+						<span class="error"><?php echo isset($erroEmail) ? $erroEmail : "";?></span>
           </div>
         </div>
         <div class="row">
           <div class="col-md-6 form-group">
             <label class="form-label-required" for="senha">Senha:</label>
             <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua senha aqui" required />
-			<span class="error"><?php echo $erroSenha;?></span>
+						<span class="error"><?php echo isset($erroSenha) ? $erroSenha : "";?></span>
           </div>
           <div class="col-md-6 form-group">
             <label class="form-label-required" for="confirmacao">Confirmar Senha:</label>
             <input type="password" class="form-control" id="confirmacao" name="confirmacao" placeholder="Repita sua senha aqui" required />
-			<span class="error"><?php echo $erroConfirmacao;?></span>
+						<span class="error"><?php echo isset($erroConfirmacao) ? $erroConfirmacao : "";?></span>
           </div>
         </div>
         <div class="row">
   		  <div class="col-md-4 form-group">
             <label class="form-label-required" for="cpf">CPF:</label>
             <input type="text" class="form-control" name="cpf" id="cpf" value="<?php echo(isset($cpf) ? $cpf : '') ?>" required />
-			<span class="error"><?php echo $erroCpf;?></span>
+						<span class="error"><?php echo isset($erroCpf) ? $erroCpf : "";?></span>
           </div>
           <div class="col-md-4 form-group">
             <label class="form-label" for="dataNascimento">Data de nascimento:</label>
             <input type="date" class="form-control" name="dataNascimento" id="dataNascimento" value="<?php echo(isset($dataNascimento) ? $dataNascimento : '') ?>" placeholder="dd/MM/yyyy" required />
-			<span class="error"><?php echo $erroDataNascimento;?></span>
+						<span class="error"><?php echo isset($erroDataNascimento) ? $erroDataNascimento : "";?></span>
           </div>
           <div class="col-md-4 form-group">
             <p class="form-label" for="sexo">Sexo:</p>
-			<label for="sexoMasculino"><input type="radio" name="sexo" id="sexoMasculino" value="M" <?php echo(($sexo === 'M' || !isset($sexo)) ? 'checked' : '') ?> />Masculino</label>
-			<label for="sexoFeminino"><input type="radio" name="sexo" id="sexoFeminino" value="F" <?php echo($sexo === 'F' ? 'checked' : '') ?> />Feminino</label>
-          </div>
+						<?php $sexo = isset($sexo) ? $sexo : 'M'; ?>
+						<label for="sexoMasculino"><input type="radio" name="sexo" id="sexoMasculino" value="M" <?php echo $sexo === 'M' ? 'checked' : '';  ?> />Masculino</label>
+						<label for="sexoFeminino"><input type="radio" name="sexo" id="sexoFeminino" value="F" <?php echo $sexo === 'F' ? 'checked' : '';  ?>/>Feminino</label>
+					</div>
         </div>
         <div class="row">
 		  <div class="col-md-4 form-group">
             <label class="form-label" for="celular">Celular:</label>
             <input type="text" class="form-control" name="celular" id="celular" value="<?php echo(isset($celular) ? $celular : '') ?>" />
-			<span class="error"><?php echo $erroCelular; ?></span>
+						<span class="error"><?php echo isset($erroCelular) ? $erroCelular : ""; ?></span>
           </div>
         </div>
         <div class="row">
   		  <div class="col-md-4 form-group">
             <label class="form-label" for="cep">CEP:</label>
             <input type="text" class="form-control" name="cep" id="cep" value="<?php echo(isset($cep) ? $cep : '') ?>" />
-			<span class="error"><?php echo $erroCep;?></span>
+						<span class="error"><?php echo isset($erroCep) ? $erroCep : "";?></span>
           </div>
           <div class="col-md-8 form-group">
             <label class="form-label" for="endereco">Endereço:</label>
@@ -214,55 +215,56 @@ include "comum/funcoes.php";
           </div>
         </div>
         <div class="row">
-		  <div class="col-md-8 form-group">
+		  	<div class="col-md-8 form-group">
             <label class="form-label" for="cidade">Cidade:</label>
             <input type="text" class="form-control" name="cidade" id="cidade" value="<?php echo(isset($cidade) ? $cidade : '') ?>" />
           </div>
           <div class="col-md-4 form-group">
+						<?php $uf = isset($uf) ? $uf : 'X'; ?>
             <label class="form-label" for="uf">Estado:</label>
             <select class="form-control" name="uf" id="uf">
-				<option value="AC" <?php echo($uf === 'AC'  ? 'selected' : '') ?>>Acre</option>
-				<option value="AL" <?php echo($uf === 'AL'  ? 'selected' : '') ?>>Alagoas</option>
-				<option value="AP" <?php echo($uf === 'AP'  ? 'selected' : '') ?>>Amapá</option>
-				<option value="AM" <?php echo($uf === 'AM'  ? 'selected' : '') ?>>Amazonas</option>
-				<option value="BA" <?php echo($uf === 'BA'  ? 'selected' : '') ?>>Baia</option>
-				<option value="CE" <?php echo($uf === 'CE'  ? 'selected' : '') ?>>Ceará</option>
-				<option value="DF" <?php echo($uf === 'DF'  ? 'selected' : '') ?>>Distrito Federal</option>
-				<option value="ES" <?php echo($uf === 'ES'  ? 'selected' : '') ?>>Espirito Santo</option>
-				<option value="GO" <?php echo($uf === 'GO'  ? 'selected' : '') ?>>Goiás</option>
-				<option value="MA" <?php echo($uf === 'MA'  ? 'selected' : '') ?>>Maranhão</option>
-				<option value="MT" <?php echo($uf === 'MT'  ? 'selected' : '') ?>>Mato Grosso</option>
-				<option value="MS" <?php echo($uf === 'MS'  ? 'selected' : '') ?>>Mato Grosso do Sul</option>
-				<option value="MG" <?php echo($uf === 'MG'  ? 'selected' : '') ?>>Minas Gerais</option>
-				<option value="PA" <?php echo($uf === 'PA'  ? 'selected' : '') ?>>Pará</option>
-				<option value="PB" <?php echo($uf === 'PB'  ? 'selected' : '') ?>>Paraiba</option>
-				<option value="PR" <?php echo($uf === 'PR'  ? 'selected' : '') ?>>Paraná</option>
-				<option value="PE" <?php echo($uf === 'PE'  ? 'selected' : '') ?>>Pernambuco</option>
-				<option value="PI" <?php echo($uf === 'PI'  ? 'selected' : '') ?>>Piauí</option>
-				<option value="RJ" <?php echo($uf === 'RJ'  ? 'selected' : '') ?>>Rio de Janeiro</option>
-				<option value="RN" <?php echo($uf === 'RN'  ? 'selected' : '') ?>>Rio Grande do Norte</option>
-				<option value="RS" <?php echo($uf === 'RS'  ? 'selected' : '') ?>>Rio Grande do Sul</option>
-				<option value="RO" <?php echo($uf === 'RO'  ? 'selected' : '') ?>>Rondônia</option>
-				<option value="RR" <?php echo($uf === 'RR'  ? 'selected' : '') ?>>Roraima</option>
-				<option value="SC" <?php echo($uf === 'SC'  ? 'selected' : '') ?>>Santa Catarina</option>
-				<option value="SE" <?php echo($uf === 'SE'  ? 'selected' : '') ?>>Sergipe</option>
-				<option value="SP" <?php echo(($uf === 'SP' || !isset($uf))  ? 'selected' : '') ?>>São Paulo</option>
-				<option value="TO" <?php echo($uf === 'TO'  ? 'selected' : '') ?>>Tocantins</option>
-			</select>
+							<option value="AC" <?php echo($uf === 'AC'  ? 'selected' : '') ?>>Acre</option>
+							<option value="AL" <?php echo($uf === 'AL'  ? 'selected' : '') ?>>Alagoas</option>
+							<option value="AP" <?php echo($uf === 'AP'  ? 'selected' : '') ?>>Amapá</option>
+							<option value="AM" <?php echo($uf === 'AM'  ? 'selected' : '') ?>>Amazonas</option>
+							<option value="BA" <?php echo($uf === 'BA'  ? 'selected' : '') ?>>Baia</option>
+							<option value="CE" <?php echo($uf === 'CE'  ? 'selected' : '') ?>>Ceará</option>
+							<option value="DF" <?php echo($uf === 'DF'  ? 'selected' : '') ?>>Distrito Federal</option>
+							<option value="ES" <?php echo($uf === 'ES'  ? 'selected' : '') ?>>Espirito Santo</option>
+							<option value="GO" <?php echo($uf === 'GO'  ? 'selected' : '') ?>>Goiás</option>
+							<option value="MA" <?php echo($uf === 'MA'  ? 'selected' : '') ?>>Maranhão</option>
+							<option value="MT" <?php echo($uf === 'MT'  ? 'selected' : '') ?>>Mato Grosso</option>
+							<option value="MS" <?php echo($uf === 'MS'  ? 'selected' : '') ?>>Mato Grosso do Sul</option>
+							<option value="MG" <?php echo($uf === 'MG'  ? 'selected' : '') ?>>Minas Gerais</option>
+							<option value="PA" <?php echo($uf === 'PA'  ? 'selected' : '') ?>>Pará</option>
+							<option value="PB" <?php echo($uf === 'PB'  ? 'selected' : '') ?>>Paraiba</option>
+							<option value="PR" <?php echo($uf === 'PR'  ? 'selected' : '') ?>>Paraná</option>
+							<option value="PE" <?php echo($uf === 'PE'  ? 'selected' : '') ?>>Pernambuco</option>
+							<option value="PI" <?php echo($uf === 'PI'  ? 'selected' : '') ?>>Piauí</option>
+							<option value="RJ" <?php echo($uf === 'RJ'  ? 'selected' : '') ?>>Rio de Janeiro</option>
+							<option value="RN" <?php echo($uf === 'RN'  ? 'selected' : '') ?>>Rio Grande do Norte</option>
+							<option value="RS" <?php echo($uf === 'RS'  ? 'selected' : '') ?>>Rio Grande do Sul</option>
+							<option value="RO" <?php echo($uf === 'RO'  ? 'selected' : '') ?>>Rondônia</option>
+							<option value="RR" <?php echo($uf === 'RR'  ? 'selected' : '') ?>>Roraima</option>
+							<option value="SC" <?php echo($uf === 'SC'  ? 'selected' : '') ?>>Santa Catarina</option>
+							<option value="SE" <?php echo($uf === 'SE'  ? 'selected' : '') ?>>Sergipe</option>
+							<option value="SP" <?php echo(($uf === 'SP' || !isset($uf))  ? 'selected' : '') ?>>São Paulo</option>
+							<option value="TO" <?php echo($uf === 'TO'  ? 'selected' : '') ?>>Tocantins</option>
+						</select>
           </div>
         </div>
         <div class="row">
           <div class="col-md-6 form-group">
-			<label class="form-label-required" for="foto">Foto:</label>
-			<input type="file" class="form-control" name="foto" id="foto" />
-			<span class="error"><?php echo $erroArquivo;?></span>
+						<label class="form-label-required" for="foto">Foto:</label>
+						<input type="file" class="form-control" name="foto" id="foto" />
+						<span class="error"><?php echo isset($erroArquivo) ? $erroArquivo : "";?></span>
           </div>
           <div class="col-md-6 form-group">
             <br/>
             <label class="form-label" for="autorizacaoContato">
-				<input type="checkbox" name="autorizacaoContato" id="autorizacaoContato" <?php echo($autorizacaoContato === 'F' ? 'checked' : '') ?> />
-				Autorizo o envio de noticias e promoções da Livraria Global em meu e-mail
-			</label>
+							<input type="checkbox" name="autorizacaoContato" id="autorizacaoContato" <?php echo(isset($autorizacaoContato) ? 'checked' : '') ?> />
+							<span>Autorizo o envio de noticias e promoções da Livraria Global em meu e-mail</span>
+						</label>
           </div>
         </div>
         <div class="row">
