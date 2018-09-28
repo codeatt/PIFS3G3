923c1273b75f80957442f7d7c74bf5d0f0320590
ojcamargo <ojcamargo@gmail.com>
2018-09-28 14:39:36 -0300

    correção mer
    correção

--- documentacao/loja.sql
+++ documentacao/loja.sql
@@ -18,41 +18,99 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `loja`
---
+-- REMOVER TABELAS
+DROP TABLE IF EXISTS `pagamento`;
+DROP TABLE IF EXISTS `MovimentoEstoque`;
+DROP TABLE IF EXISTS `MovimentoOrigem`;
+DROP TABLE IF EXISTS `vendaitem`;
+DROP TABLE IF EXISTS `venda`;
+DROP TABLE IF EXISTS `livro`;
+DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `clienteendereco`;
+DROP TABLE IF EXISTS `clientecontato`;
+DROP TABLE IF EXISTS `cliente`;
+DROP TABLE IF EXISTS `tipocontato`;
+DROP TABLE IF EXISTS `pagamentostatus`;
+DROP TABLE IF EXISTS `vendastatus`;
+DROP TABLE IF EXISTS `editora`;
+DROP TABLE IF EXISTS `categoria`;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `categoria`
---
+-- CATEGORIA
 
-CREATE TABLE IF NOT EXISTS  `categoria` (
+CREATE TABLE `categoria` (
   `CategoriaId` int(11) NOT NULL,
   `Descricao` varchar(50) NOT NULL,
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`CategoriaId`),
+  ADD UNIQUE KEY `Descricao` (`Descricao`);
+  
+ALTER TABLE `categoria`
+  MODIFY `CategoriaId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- EDITORA
 
---
--- Estrutura da tabela `editora`
---
-
-CREATE TABLE IF NOT EXISTS  `editora` (
+CREATE TABLE `editora` (
   `EditoraId` int(11) NOT NULL,
   `Nome` varchar(50) NOT NULL,
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `editora`
+  ADD PRIMARY KEY (`EditoraId`),
+  ADD UNIQUE KEY `Nome` (`Nome`);
+  
+ALTER TABLE `editora`
+  MODIFY `EditoraId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- VENDASTATUS
 
---
--- Estrutura da tabela `cliente`
---
+CREATE TABLE `vendastatus` (
+  `VendaStatusId` int(11) NOT NULL,
+  `Descricao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS  `cliente` (
+ALTER TABLE `vendastatus`
+  ADD PRIMARY KEY (`VendaStatusId`),
+  ADD UNIQUE KEY `Descricao` (`Descricao`);
+  
+ALTER TABLE `vendastatus`
+  MODIFY `VendaStatusId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- PAGAMENTOSTATUS
+
+CREATE TABLE `pagamentostatus` (
+  `PagamentoStatusId` int(11) NOT NULL,
+  `Descricao` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `pagamentostatus`
+  ADD PRIMARY KEY (`PagamentoStatusId`),
+  ADD UNIQUE KEY `Descricao` (`Descricao`);
+  
+ALTER TABLE `pagamentostatus`
+  MODIFY `PagamentoStatusId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- TIPOCONTATO
+
+CREATE TABLE `tipocontato` (
+  `TipoContatoId` int(11) NOT NULL,
+  `Descricao` varchar(20) NOT NULL,
+  `Ativo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `tipocontato`
+  ADD PRIMARY KEY (`TipoContatoId`);
+  
+ALTER TABLE `tipocontato`
+  MODIFY `TipoContatoId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- CLIENTE
+
+CREATE TABLE `cliente` (
   `ClienteId` int(11) NOT NULL,
   `Nome` varchar(50) NOT NULL,
   `CPF` varchar(11) NOT NULL,
@@ -64,66 +122,17 @@ CREATE TABLE IF NOT EXISTS  `cliente` (
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ClienteId`),
+  ADD UNIQUE KEY `UK_Cliente_Nome` (`Nome`),
+  ADD UNIQUE KEY `UK_Cliente_CPF` (`CPF`);
+  
+ALTER TABLE `cliente`
+  MODIFY `ClienteId` int(11) NOT NULL AUTO_INCREMENT;
+  
+-- CLIENTECONTATO
 
---
--- Estrutura da tabela `livro`
---
-
-CREATE TABLE IF NOT EXISTS  `livro` (
-  `LivroId` int(11) NOT NULL,
-  `Titulo` varchar(50) NOT NULL,
-  `Autor` varchar(30) NOT NULL,
-  `Preco` float(7,2) NOT NULL,
-  `QtdEstoque` int(11) NOT NULL,
-  `Edicao` varchar(20) NOT NULL,
-  `Ativo` tinyint(1) NOT NULL,
-  `EditoraId` int(11) NOT NULL,
-  `CategoriaId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `vendastatus`
---
-
-CREATE TABLE IF NOT EXISTS  `vendastatus` (
-  `VendaStatusId` int(11) NOT NULL,
-  `Descricao` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pagamentostatus`
---
-
-CREATE TABLE IF NOT EXISTS  `pagamentostatus` (
-  `PagamentoStatusId` int(11) NOT NULL,
-  `Descricao` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tipocontato`
---
-
-CREATE TABLE IF NOT EXISTS  `tipocontato` (
-  `TipoContatoId` int(11) NOT NULL,
-  `Descricao` varchar(20) NOT NULL,
-  `Ativo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `clientecontato`
---
-
-CREATE TABLE IF NOT EXISTS  `clientecontato` (
+CREATE TABLE `clientecontato` (
   `ClienteContatoId` int(11) NOT NULL,
   `ClienteId` int(11) NOT NULL,
   `TipoContatoId` int(11) NOT NULL,
@@ -131,13 +140,21 @@ CREATE TABLE IF NOT EXISTS  `clientecontato` (
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `clientecontato`
+  ADD PRIMARY KEY (`ClienteContatoId`),
+  ADD KEY `FK_Contato_Cliente` (`ClienteId`),
+  ADD KEY `FK_Contato_Tipo` (`TipoContatoId`);
+  
+ALTER TABLE `clientecontato`
+  MODIFY `ClienteContatoId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `clientecontato`
+  ADD CONSTRAINT `FK_Contato_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`),
+  ADD CONSTRAINT `FK_Contato_Tipo` FOREIGN KEY (`TipoContatoId`) REFERENCES `tipocontato` (`TipoContatoId`);
+  
+-- CLIENTEENDERECO
 
---
--- Estrutura da tabela `clienteendereco`
---
-
-CREATE TABLE IF NOT EXISTS  `clienteendereco` (
+CREATE TABLE `clienteendereco` (
   `ClienteEnderecoId` int(11) NOT NULL,
   `ClienteId` int(11) NOT NULL,
   `Logradouro` varchar(50) NOT NULL,
@@ -151,13 +168,67 @@ CREATE TABLE IF NOT EXISTS  `clienteendereco` (
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `clienteendereco`
+  ADD PRIMARY KEY (`ClienteEnderecoId`),
+  ADD KEY `FK_Endereco_Cliente` (`ClienteId`);
+  
+ALTER TABLE `clienteendereco`
+  MODIFY `ClienteEnderecoId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `clienteendereco`
+  ADD CONSTRAINT `FK_Endereco_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`);
+  
+-- USUARIO
 
---
--- Estrutura da tabela `venda`
---
+CREATE TABLE `usuario` (
+  `UsuarioId` int(11) NOT NULL,
+  `Login` varchar(20) NOT NULL,
+  `Senha` varchar(20) NOT NULL,
+  `ClienteId` int(11) NOT NULL,
+  `Ativo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS  `venda` (
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`UsuarioId`),
+  ADD UNIQUE KEY `UK_Usuario_Login` (`Login`),
+  ADD KEY `FK_Usuario_Cliente` (`ClienteId`);
+  
+ALTER TABLE `usuario`
+  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `FK_Usuario_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`);
+  
+-- LIVRO
+
+CREATE TABLE `livro` (
+  `LivroId` int(11) NOT NULL,
+  `Titulo` varchar(50) NOT NULL,
+  `Autor` varchar(30) NOT NULL,
+  `Preco` float(7,2) NOT NULL,
+  `QtdEstoque` int(11) NOT NULL,
+  `Edicao` varchar(20) NOT NULL,
+  `Ativo` tinyint(1) NOT NULL,
+  `EditoraId` int(11) NOT NULL,
+  `CategoriaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`LivroId`),
+  ADD UNIQUE KEY `Titulo` (`Titulo`),
+  ADD KEY `FK_Livro_Editora` (`EditoraId`),
+  ADD KEY `FK_Livro_Categoria` (`CategoriaId`);
+  
+ALTER TABLE `livro`
+  MODIFY `LivroId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `livro`
+  ADD CONSTRAINT `FK_Livro_Categoria` FOREIGN KEY (`CategoriaId`) REFERENCES `categoria` (`CategoriaId`),
+  ADD CONSTRAINT `FK_Livro_Editora` FOREIGN KEY (`EditoraId`) REFERENCES `editora` (`EditoraId`);
+  
+-- VENDA
+
+CREATE TABLE `venda` (
   `PedidoId` int(11) NOT NULL,
   `VendaStatusId` int(11) NOT NULL,
   `ClienteId` int(11) NOT NULL,
@@ -169,68 +240,23 @@ CREATE TABLE IF NOT EXISTS  `venda` (
   `ValorDesconto` decimal(7,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`PedidoId`),
+  ADD KEY `FK_Venda_Status` (`VendaStatusId`),
+  ADD KEY `FK_Venda_Cliente` (`ClienteId`),
+  ADD KEY `FK_Venda_ClienteEndereco` (`ClienteEnderecoId`);
+  
+ALTER TABLE `venda`
+  MODIFY `PedidoId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `venda`
+  ADD CONSTRAINT `FK_Venda_Status` FOREIGN KEY (`VendaStatusId`) REFERENCES `vendastatus` (`VendaStatusId`),
+  ADD CONSTRAINT `FK_Venda_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`),
+  ADD CONSTRAINT `FK_Venda_ClienteEndereco` FOREIGN KEY (`ClienteEnderecoId`) REFERENCES `clienteendereco` (`ClienteEnderecoId`);
+  
+-- VENDAITEM
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `estoque`
---
-
-CREATE TABLE IF NOT EXISTS  `estoque` (
-  `EstoqueId` int(11) NOT NULL,
-  `LivroId` int(11) NOT NULL,
-  `Data` int(11) NOT NULL,
-  `Quantidade` int(11) NOT NULL,
-  `MovimentoId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `movimento`
---
-
-CREATE TABLE IF NOT EXISTS  `movimento` (
-  `MovimentoId` int(11) NOT NULL,
-  `Motivo` varchar(30) NOT NULL,
-  `TipoMovimento` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pagamento`
---
-
-CREATE TABLE IF NOT EXISTS  `pagamento` (
-  `PagamentoId` int(11) NOT NULL,
-  `PedidoId` int(11) NOT NULL,
-  `PagamentoStatusId` int(11) NOT NULL,
-  `DataPagamento` datetime NOT NULL,
-  `Observacao` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE IF NOT EXISTS  `usuario` (
-  `UsuarioId` int(11) NOT NULL,
-  `Login` varchar(20) NOT NULL,
-  `Senha` varchar(20) NOT NULL,
-  `ClienteId` int(11) NOT NULL,
-  `Ativo` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `vendaitem`
---
-
-CREATE TABLE IF NOT EXISTS  `vendaitem` (
+CREATE TABLE `vendaitem` (
   `ItemId` int(11) NOT NULL,
   `LivroId` int(11) NOT NULL,
   `PedidoId` int(11) NOT NULL,
@@ -238,240 +264,77 @@ CREATE TABLE IF NOT EXISTS  `vendaitem` (
   `Quantidade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+ALTER TABLE `vendaitem`
+  ADD PRIMARY KEY (`ItemId`),
+  ADD KEY `FK_Item_Livro` (`LivroId`),
+  ADD KEY `FK_Item_Venda` (`PedidoId`);
+  
+ALTER TABLE `vendaitem`
+  MODIFY `ItemId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `vendaitem`
+  ADD CONSTRAINT `FK_Item_Livro` FOREIGN KEY (`LivroId`) REFERENCES `livro` (`LivroId`),
+  ADD CONSTRAINT `FK_Item_Venda` FOREIGN KEY (`PedidoId`) REFERENCES `venda` (`PedidoId`);
+  
+-- MOVIMENTOORIGEM
 
---
--- Indexes for table `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`CategoriaId`),
-  ADD UNIQUE KEY `Descricao` (`Descricao`);
+CREATE TABLE `MovimentoOrigem` (
+  `MovimentoOrigemId` int(11) NOT NULL,
+  `Origem` varchar(30) NOT NULL,
+  `TipoMovimento` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`ClienteId`),
-  ADD UNIQUE KEY `UK_Cliente_Nome` (`Nome`),
-  ADD UNIQUE KEY `UK_Cliente_CPF` (`CPF`);
+ALTER TABLE `MovimentoOrigem`
+  ADD PRIMARY KEY (`MovimentoOrigemId`),
+  ADD UNIQUE KEY `Origem` (`Origem`);
+  
+ALTER TABLE `MovimentoOrigem`
+  MODIFY `MovimentoOrigemId` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Indexes for table `clientecontato`
---
-ALTER TABLE `clientecontato`
-  ADD PRIMARY KEY (`ClienteContatoId`),
-  ADD KEY `FK_Contato_Cliente` (`ClienteId`),
-  ADD KEY `FK_Contato_Tipo` (`TipoContatoId`);
+-- MOVIMENTOESTOQUE
 
---
--- Indexes for table `clienteendereco`
---
-ALTER TABLE `clienteendereco`
-  ADD PRIMARY KEY (`ClienteEnderecoId`),
-  ADD KEY `FK_Endereco_Cliente` (`ClienteId`);
+CREATE TABLE `MovimentoEstoque` (
+  `MovimentoEstoqueId` int(11) NOT NULL,
+  `LivroId` int(11) NOT NULL,
+  `Data` int(11) NOT NULL,
+  `Quantidade` int(11) NOT NULL,
+  `MovimentoOrigemId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `editora`
---
-ALTER TABLE `editora`
-  ADD PRIMARY KEY (`EditoraId`),
-  ADD UNIQUE KEY `Nome` (`Nome`);
+ALTER TABLE `MovimentoEstoque`
+  ADD PRIMARY KEY (`MovimentoEstoqueId`),
+  ADD KEY `FK_MovimentoEstoque_Origem` (`MovimentoOrigemId`),
+  ADD KEY `FK_MovimentoEstoque_Livro` (`LivroId`);
+  
+ALTER TABLE `MovimentoEstoque`
+  MODIFY `MovimentoEstoqueId` int(11) NOT NULL AUTO_INCREMENT;
+  
+ALTER TABLE `MovimentoEstoque`
+  ADD CONSTRAINT `FK_MovimentoEstoque_Origem` FOREIGN KEY (`MovimentoOrigemId`) REFERENCES `MovimentoOrigem` (`MovimentoOrigemId`),
+  ADD CONSTRAINT `FK_MovimentoEstoque_Livro` FOREIGN KEY (`LivroId`) REFERENCES `livro` (`LivroId`);
 
---
--- Indexes for table `livro`
---
-ALTER TABLE `livro`
-  ADD PRIMARY KEY (`LivroId`),
-  ADD UNIQUE KEY `Titulo` (`Titulo`),
-  ADD KEY `FK_Livro_Editora` (`EditoraId`),
-  ADD KEY `FK_Livro_Categoria` (`CategoriaId`);
+-- PAGAMENTO
 
---
--- Indexes for table `pagamento`
---
+CREATE TABLE `pagamento` (
+  `PagamentoId` int(11) NOT NULL,
+  `PedidoId` int(11) NOT NULL,
+  `PagamentoStatusId` int(11) NOT NULL,
+  `DataPagamento` datetime NOT NULL,
+  `Observacao` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 ALTER TABLE `pagamento`
   ADD PRIMARY KEY (`PagamentoId`),
   ADD KEY `FK_Pagamento_Venda` (`PedidoId`),
   ADD KEY `FK_Pagamento_Status` (`PagamentoStatusId`);
-
---
--- Indexes for table `pagamentostatus`
---
-ALTER TABLE `pagamentostatus`
-  ADD PRIMARY KEY (`PagamentoStatusId`),
-  ADD UNIQUE KEY `Descricao` (`Descricao`);
-
---
--- Indexes for table `tipocontato`
---
-ALTER TABLE `tipocontato`
-  ADD PRIMARY KEY (`TipoContatoId`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`UsuarioId`),
-  ADD UNIQUE KEY `UK_Usuario_Login` (`Login`),
-  ADD KEY `FK_Usuario_Cliente` (`ClienteId`);
-
---
--- Indexes for table `venda`
---
-ALTER TABLE `venda`
-  ADD PRIMARY KEY (`PedidoId`),
-  ADD KEY `FK_Venda_Cliente` (`ClienteId`),
-  ADD KEY `FK_Venda_ClienteEndereco` (`ClienteEnderecoId`);
-
---
--- Indexes for table `vendaitem`
---
-ALTER TABLE `vendaitem`
-  ADD PRIMARY KEY (`ItemId`);
-  ADD KEY `FK_Item_Livro` (`LivrId`),
-  ADD KEY `FK_Item_Venda` (`PedidoId`);
-
---
--- Indexes for table `vendastatus`
---
-ALTER TABLE `vendastatus`
-  ADD PRIMARY KEY (`VendaStatusId`),
-  ADD UNIQUE KEY `Descricao` (`Descricao`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `CategoriaId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `ClienteId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `clientecontato`
---
-ALTER TABLE `clientecontato`
-  MODIFY `ClienteContatoId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `clienteendereco`
---
-ALTER TABLE `clienteendereco`
-  MODIFY `ClienteEnderecoId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `editora`
---
-ALTER TABLE `editora`
-  MODIFY `EditoraId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `livro`
---
-ALTER TABLE `livro`
-  MODIFY `LivroId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pagamento`
---
+  
 ALTER TABLE `pagamento`
   MODIFY `PagamentoId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pagamentostatus`
---
-ALTER TABLE `pagamentostatus`
-  MODIFY `PagamentoStatusId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tipocontato`
---
-ALTER TABLE `tipocontato`
-  MODIFY `TipoContatoId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `UsuarioId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `venda`
---
-ALTER TABLE `venda`
-  MODIFY `PedidoId` int(11) NOT NULL AUTO_INCREMENT;
   
---
--- AUTO_INCREMENT for table `vendaitem`
---
-ALTER TABLE `vendaitem`
-  MODIFY `ItemId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vendastatus`
---
-ALTER TABLE `vendastatus`
-  MODIFY `VendaStatusId` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `clientecontato`
---
-ALTER TABLE `clientecontato`
-  ADD CONSTRAINT `FK_Contato_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`),
-  ADD CONSTRAINT `FK_Contato_Tipo` FOREIGN KEY (`TipoContatoId`) REFERENCES `tipocontato` (`TipoContatoId`);
-
---
--- Limitadores para a tabela `clienteendereco`
---
-ALTER TABLE `clienteendereco`
-  ADD CONSTRAINT `FK_Endereco_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`);
-
---
--- Limitadores para a tabela `livro`
---
-ALTER TABLE `livro`
-  ADD CONSTRAINT `FK_Livro_Categoria` FOREIGN KEY (`CategoriaId`) REFERENCES `categoria` (`CategoriaId`),
-  ADD CONSTRAINT `FK_Livro_Editora` FOREIGN KEY (`EditoraId`) REFERENCES `editora` (`EditoraId`);
-
---
--- Limitadores para a tabela `pagamento`
---
 ALTER TABLE `pagamento`
   ADD CONSTRAINT `FK_Pagamento_Status` FOREIGN KEY (`PagamentoStatusId`) REFERENCES `pagamentostatus` (`PagamentoStatusId`),
   ADD CONSTRAINT `FK_Pagamento_Venda` FOREIGN KEY (`PedidoId`) REFERENCES `venda` (`PedidoId`);
 
---
--- Limitadores para a tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `FK_Usuario_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`);
-
---
--- Limitadores para a tabela `venda`
---
-ALTER TABLE `venda`
-  ADD CONSTRAINT `FK_Venda_Cliente` FOREIGN KEY (`ClienteId`) REFERENCES `cliente` (`ClienteId`),
-  ADD CONSTRAINT `FK_Venda_ClienteEndereco` FOREIGN KEY (`ClienteEnderecoId`) REFERENCES `clienteendereco` (`ClienteEnderecoId`);
-
---
--- Limitadores para a tabela `vendaitem`
---
-ALTER TABLE `vendaitem`
-  ADD CONSTRAINT `FK_Item_Livro` FOREIGN KEY (`LivroId`) REFERENCES `livro` (`LivroId`),
-  ADD CONSTRAINT `FK_Item_Venda` FOREIGN KEY (`PedidoId`) REFERENCES `venda` (`PedidoId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
